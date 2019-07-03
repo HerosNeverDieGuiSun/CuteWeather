379314bb84cb27bc9b48ad6bba9ab2162f9be1e1
zzd <949759694@qq.com>
2019-07-03 09:22:34 +0800

    初始

---
+++ app/src/main/java/com/example/cuteweather/AirActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cuteweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AirActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_air)
+    }
+}
